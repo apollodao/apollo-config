b1280985ae3110fe6a5e2f73fb57d1f631d9767b
othermike <othermike@apollo.farm>
2023-02-16 21:57:11 -0500

    feat: add new 14d vaults

--- src/config.ts
+++ src/config.ts
@@ -287,6 +287,61 @@ export const config: NetworkConfig = {
           "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
         code_id: 17,
       },
+
+      {
+        label: "USDC-OSMO 14d",
+        description: "USDC-OSMO Autocompounding Vault 14d",
+        address:
+          "osmo1jfmwayj8jqp9tfy4v4eks5c2jpnqdumn8x8xvfllng0wfes770qqp7jl4j",
+        admin:
+          "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
+        code_id: 17,
+      },
+      {
+        label: "WBTC-OSMO 14d",
+        description: "WBTC-OSMO Autocompounding Vault 14d",
+        address:
+          "osmo185gqewrlde8vrqw7j8lpad67v8jfrx9u7770k9q87tqqecctp5tq50wt2c",
+        admin:
+          "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
+        code_id: 17,
+      },
+      {
+        label: "DAI-OSMO 14d",
+        description: "DAI-OSMO Autocompounding Vault 14d",
+        address:
+          "osmo1lhs6kyuxytu4suua0qf88z5057wzpxs77tyrlgztw2uctcy75hcqf2ajrt",
+        admin:
+          "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
+        code_id: 17,
+      },
+      {
+        label: "stOSMO-OSMO 14d",
+        description: "stOSMO-OSMO Autocompounding Vault 14d",
+        address:
+          "osmo1p4zqs5y2w5srzd2vesznzu5ql8wfq9tpz3e7mf2j3y07nxrtkdes5r5g0t",
+        admin:
+          "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
+        code_id: 17,
+      },
+      {
+        label: "wETH-OSMO 14d",
+        description: "wETH-OSMO Autocompounding Vault 14d",
+        address:
+          "osmo1r235f4tdkwrsnj3mdm9hf647l754y6g6xsmz0nas5r4vr5tda3qsgtftef",
+        admin:
+          "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
+        code_id: 17,
+      },
+      {
+        label: "ATOM-stATOM 14d",
+        description: "ATOM-stATOM Autocompounding Vault 14d",
+        address:
+          "osmo1a6tcf60pyz8qq2n532dzcs7s7sj8klcmra04tvaqympzcvxqg9esn7xz7l",
+        admin:
+          "osmo1wl59k23zngj34l7d42y9yltask7rjlnxgccawc7ltrknp6n52fps94qsjd",
+        code_id: 17,
+      },
     ],
   },
   "pulsar-2": {
