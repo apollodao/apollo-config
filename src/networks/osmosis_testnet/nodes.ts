403be11d8edbc2bbddb06ddcac59791c4893c199
shab <dirtyshab@protonmail.com>
2023-03-04 09:28:41 -0500

    feat: osmosis testnet nodes

--- src/networks/osmosis_testnet/nodes.ts
+++ src/networks/osmosis_testnet/nodes.ts
@@ -1,3 +1,14 @@
 import { Node } from "../../types";
 
-export const nodes: Node[] = [];
+export const nodes: Node[] = [
+  {
+    name: "allthatnode",
+    rpc: "https://osmosis-testnet-rpc.allthatnode.com:26657",
+    lcd: "https://osmosis-testnet-rpc.allthatnode.com:1317",
+  },
+  {
+    name: "osmosis",
+    rpc: "https://rpc-test.osmosis.zone:443",
+    lcd: "https://testnet-rest.osmosis.zone",
+  },
+];
