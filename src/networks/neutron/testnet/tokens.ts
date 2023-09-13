daeac7c23bb0901c93ba032052ad160c89b4f40b
shab <dirtyshab@protonmail.com>
2023-09-13 18:30:55 -0400

    fix: optional base_token and underlying_tokens in neutron token params

--- src/networks/neutron/testnet/tokens.ts
+++ src/networks/neutron/testnet/tokens.ts
@@ -5,6 +5,8 @@ export type supported_data_sources = {
   params: {
     dex: "astroport";
     contract_address: string;
+    base_token?: AssetInfo;
+    underlying_tokens?: AssetInfo[];
   };
 };
 
@@ -28,6 +30,9 @@ export const tokens: (Omit<Token, "sources"> & {
           dex: "astroport",
           contract_address:
             "neutron1675p0u4eflqgvfwf3tlk8rhkvfjggrenjlc4jqtyjgsrfp9kyrus6r8hly",
+          base_token: {
+            cw20: "neutron1cxzfrnpjpptlwpwz6x0c4s5ephzxf0xm32p8tau2y5l2k2qv6e9sjznux9",
+          },
         },
       },
     ],
@@ -50,6 +55,9 @@ export const tokens: (Omit<Token, "sources"> & {
           dex: "astroport",
           contract_address:
             "neutron10r7q0pv5ms8t7pspuy0vry5mc38sfxh8nvmn5qm6cp30m409ln7qgnrx7v",
+          base_token: {
+            cw20: "neutron1tur4sa9y2jqnvmepd80qwdykr3r73flr3a39kpm7frqwnh98c00s3w6v5w",
+          },
         },
       },
     ],
@@ -75,6 +83,13 @@ export const tokens: (Omit<Token, "sources"> & {
           dex: "astroport",
           contract_address:
             "neutron1vwrktvvxnevy7s5t7v44z72pdxncnq9gdsjwq9607cdd6vl2lfcs33fpah",
+          underlying_tokens: [
+            { native: "untrn" },
+            {
+              native:
+                "ibc/EFB00E728F98F0C4BBE8CA362123ACAB466EDA2826DC6837E49F4C1902F21BBA",
+            },
+          ],
         },
       },
     ],
@@ -100,6 +115,13 @@ export const tokens: (Omit<Token, "sources"> & {
           dex: "astroport",
           contract_address:
             "neutron1ppl8ku9n3vxl27npmkr9a9j4v950wduuuy8rmhqs8zf4c87qr37q98sw7c",
+          underlying_tokens: [
+            { native: "untrn" },
+            {
+              native:
+                "ibc/85CE72EE820A66F0ABD5EE3907A34E243E4BE2D6CFAEB4C08DF85BD6C0784FA2",
+            },
+          ],
         },
       },
     ],
