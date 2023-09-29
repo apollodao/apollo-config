42a89addfb69acf267ec4091db6c579fd34427bb
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-09-29 17:25:06 +0200

    feat: Update neutron vault labels and descriptions

--- src/networks/neutron/mainnet/contracts.ts
+++ src/networks/neutron/mainnet/contracts.ts
@@ -2,20 +2,20 @@ import { Contract } from "../../../types";
 
 export const contracts: Contract[] = [
   // Currently no rewards for AXL/NTRN
-  // {
-  //   label: "AXL/NTRN 0d",
-  //   description: "AXL/NTRN Unlocked Autocompounding Vault",
-  //   address:
-  //     "neutron12pdx3z009fx92kcsr8fhvnkh92w98n5f5xaecz3sn0kzxthz20js0llxjw",
-  //   admin: "neutron16u56nw6rhdt5wc63ammgdn565q6t825mpunm35",
-  //   code_id: 266,
-  // },
   {
-    label: "wstETH/axlWETH 0d",
-    description: "wstETH/axlWETH Unlocked Autocompounding Vault",
+    label: "AXL/NTRN Unlocked LP Vault",
+    description: "This vault takes in AXL/NTRN Astroport LP tokens and autocompounds any rewards back into the LP position. There is no lockup period for this vault and funds can be withdrawn at any time.",
+    address:
+      "neutron12pdx3z009fx92kcsr8fhvnkh92w98n5f5xaecz3sn0kzxthz20js0llxjw",
+    admin: "neutron16u56nw6rhdt5wc63ammgdn565q6t825mpunm35",
+    code_id: 266,
+  },
+  {
+    label: "wstETH/axlWETH Unlocked LP Vault",
+    description: "This vault takes in wstETH/axlWETH Astroport LP tokens and autocompounds any rewards back into the LP position. There is no lockup period for this vault and funds can be withdrawn at any time.",
     address:
       "neutron1jyk9sulr5wfyy0zp95cujupvennnc9xap79wkp6pwp7k2qmsmz2qw0wkrg",
     admin: "neutron16u56nw6rhdt5wc63ammgdn565q6t825mpunm35",
-    code_id: 266,
+    code_id: 278,
   },
 ];
