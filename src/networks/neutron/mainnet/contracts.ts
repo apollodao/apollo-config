0026b07e15dc4cb56809fcf89fad657fd2834dc5
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-12-12 17:40:58 +0100

    feat: add NTRN/wstETH vault

--- src/networks/neutron/mainnet/contracts.ts
+++ src/networks/neutron/mainnet/contracts.ts
@@ -6,7 +6,7 @@ export const contracts: Contract[] = [
     description: "This vault takes in ASTRO/axlUSDC Astroport LP tokens and autocompounds any rewards back into the LP position. There is no lockup period for this vault and funds can be withdrawn at any time.",
     address:
       "neutron135nkp0fth0vtertv7ngvkkgc4cwamp2tpnmjdlppat0047f9wjmqxeu9p8",
-    admin: "neutron16u56nw6rhdt5wc63ammgdn565q6t825mpunm35",
+    admin: "neutron1qnpwxhrgd8mmsfgql7df6kusgjr3wvm4trl05xu260seelwh845qtqqq9t",
     code_id: 451,
   },
   {
@@ -14,7 +14,15 @@ export const contracts: Contract[] = [
     description: "This vault takes in wstETH/axlWETH Astroport PCL LP tokens and autocompounds any rewards back into the LP position. Funds are locked for 7 days after withdrawal.",
     address:
       "neutron1yvhe4f0q3swtf37pkf9kku59l52nevr3trxs62vah004a08pkl8qlaccc7",
-    admin: "neutron16u56nw6rhdt5wc63ammgdn565q6t825mpunm35",
+    admin: "neutron1qnpwxhrgd8mmsfgql7df6kusgjr3wvm4trl05xu260seelwh845qtqqq9t",
+    code_id: 451,
+  },
+  {
+    label: "NTRN/wstETH 7d Locked LP Vault",
+    description: "This vault takes in NTRN/wstETH Astroport PCL LP tokens and autocompounds any rewards back into the LP position. Funds are locked for 7 days after withdrawal.",
+    address:
+      "neutron17vedy2clhctw0654k93m375ud7h5jsy8nj9gnlkjnyd4mcfnfrdql226al",
+    admin: "neutron1qnpwxhrgd8mmsfgql7df6kusgjr3wvm4trl05xu260seelwh845qtqqq9t",
     code_id: 451,
   }
 ];
--- src/networks/neutron/mainnet/tokens.ts
+++ src/networks/neutron/mainnet/tokens.ts
@@ -67,6 +67,31 @@ export const tokens: (Omit<Token, "sources"> & {
     decimals: 12,
   },
   {
+    name: "NTRN/wstETH 7d Vault Token",
+    img_url: "",
+    asset: {
+      native:
+        "factory/neutron17vedy2clhctw0654k93m375ud7h5jsy8nj9gnlkjnyd4mcfnfrdql226al/VaultToken/pcl/7d/NTRN/wstETH",
+    },
+    base: {
+      cw20: "neutron15ww380rfn7r479tdyxpxa2mg2m8u85vlvdtr5tschh9q8s02gu8s7vr4dc",
+    },
+    sources: [
+      {
+        name: "neutron-1",
+        params: {
+          dex: "astroport",
+          contract_address:
+            "neutron17vedy2clhctw0654k93m375ud7h5jsy8nj9gnlkjnyd4mcfnfrdql226al",
+          base_token: {
+            cw20: "neutron15ww380rfn7r479tdyxpxa2mg2m8u85vlvdtr5tschh9q8s02gu8s7vr4dc",
+          },
+        },
+      },
+    ],
+    decimals: 12,
+  },
+  {
     name: "ASTRO/axlUSDC LP Token",
     img_url: "",
     asset: {
@@ -151,6 +176,45 @@ export const tokens: (Omit<Token, "sources"> & {
             "neutron1yw0a7nxa8jdgzmdsme4gwxhj76n44z305qgwrzvlfavgna9epcys3k9m2f",
           reward_contract:
             "neutron1addt5qylptc0f9343q2u7q5wqulnwl5fwyqss9f757nrw34d0dzqvf8l2g",
+          underlying_tokens: [
+            {
+              native:
+                "factory/neutron1ug740qrkquxzrk2hh29qrlx3sktkfml3je7juusc2te7xmvsscns0n2wry/wstETH",
+            },
+            {
+              native:
+                "ibc/A585C2D15DCD3B010849B453A2CFCB5E213208A5AB665691792684C26274304D",
+            },
+          ],
+          swap_fee: 0.003,
+        },
+      },
+    ],
+    decimals: 6,
+  },
+  {
+    name: "NTRN/wstETH LP Token",
+    img_url: "",
+    asset: {
+      cw20: "neutron15ww380rfn7r479tdyxpxa2mg2m8u85vlvdtr5tschh9q8s02gu8s7vr4dc",
+    },
+    base: [
+      {
+        native:
+          "factory/neutron1ug740qrkquxzrk2hh29qrlx3sktkfml3je7juusc2te7xmvsscns0n2wry/wstETH",
+      },
+      {
+        native:
+          "untrn",
+      },
+    ],
+    sources: [
+      {
+        name: "neutron-1",
+        params: {
+          dex: "astroport",
+          contract_address:
+            "neutron1fr3h0w6sg62agzpvu42ewj8m6rn9x45py77egd8xefp2682st39suqnx6n",
           reward_token: {
             cw20: "neutron149tpx0nequ0cpctw06nk29hwfps0343wvwfh6tkhju3ra7hxvpeq8pvstf",
           },
@@ -161,7 +225,7 @@ export const tokens: (Omit<Token, "sources"> & {
             },
             {
               native:
-                "ibc/A585C2D15DCD3B010849B453A2CFCB5E213208A5AB665691792684C26274304D",
+                "untrn",
             },
           ],
           swap_fee: 0.003,
